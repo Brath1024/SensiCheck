f4fd53e55ae3f7efa990a51864b77e2b9056d10a
brath <email_guoqing@163.com>
2023-12-27 21:44:41 +0800

    feat: sensicheck init

---
+++ src/test/java/cn/brath/sensicheck/SensiCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.brath.sensicheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensiCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
